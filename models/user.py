bdf050afbe6659fd9cf87aa8ac57665ca967e708
Macktireh <abdimack97@gmail.com>
2022-12-05 23:36:08 +0100

    upload image profile

--- models/user.py
+++ models/user.py
@@ -5,11 +5,16 @@ from flask_login import UserMixin
 from app import db, flask_bcrypt
 
 
-# user_userImage = db.Table(
-#         'user_userImage',
-#         db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
-#         db.Column('userImage_id', db.Integer, db.ForeignKey('userImage.id'))
-#     )
+class UserImage(db.Model):
+    """ UserImage Model for profile picture registration """
+    __tablename__ = "userImage"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    publicId = db.Column(db.Text, unique=True, nullable=False)
+    image = db.Column(db.Text, nullable=False)
+    name = db.Column(db.Text, nullable=False)
+    nimeType = db.Column(db.Text, nullable=False)
+    userId = db.Column(db.Integer, db.ForeignKey("user.id"))
 
 
 class User(db.Model, UserMixin):
@@ -17,7 +22,7 @@ class User(db.Model, UserMixin):
     __tablename__ = "user"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    publicId = db.Column(db.Text, unique=True)
+    publicId = db.Column(db.Text, unique=True, nullable=False)
     email = db.Column(db.String(255), unique=True, nullable=False)
     firstName = db.Column(db.String(128))
     lastName = db.Column(db.String(128))
@@ -27,7 +32,7 @@ class User(db.Model, UserMixin):
     createdAt = db.Column(db.DateTime, nullable=False, default=datetime.now)
     updatedAt = db.Column(db.DateTime, nullable=False, default=datetime.now)
     passwordHash = db.Column(db.Text, nullable=False)
-    image_id = db.Column(db.Integer, db.ForeignKey('userImage.id'))
+    image = db.relationship(UserImage, backref="user", cascade="all, delete, delete-orphan", single_parent=True, uselist=False)
     # picture = db.relationship('UserImage', secondary=user_userImage, backref='users')
 
 
@@ -47,15 +52,3 @@ class User(db.Model, UserMixin):
 
     def __repr__(self):
         return "<User '{}'>".format(self.email)
-
-
-class UserImage(db.Model):
-    """ UserImage Model for profile picture registration """
-    __tablename__ = "userImage"
-
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    publicId = db.Column(db.Text, unique=True)
-    image = db.Column(db.Text, nullable=False)
-    name = db.Column(db.Text, nullable=False)
-    nimeType = db.Column(db.Text, nullable=False)
-    user = db.relationship('User', uselist=False, backref='image')
