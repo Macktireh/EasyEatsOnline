4a0362ba39321f391547f9f64570f12a82189d56
Macktireh <abdimack97@gmail.com>
2022-12-31 18:12:28 +0100

    fix github action workflow

--- config/settings.py
+++ config/settings.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-load = load_dotenv(os.path.join(BASE_DIR, '.env'))
+load = load_dotenv(os.path.join(BASE_DIR, '.env.example'))
 
 
 class GlobalConfig:
